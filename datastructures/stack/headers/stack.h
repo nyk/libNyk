0e491613d1cab94d1f1fba3bdeb2f587f769a65b
Nyk Cowham <nyk@cowham.net>
2021-04-05 13:16:01 +0700

    Initial sketch of functions and layout for stack library

---
+++ datastructures/stack/headers/stack.h
@@ -0,0 +1,65 @@
+/**
+ * @file stack.h
+ * @author Nyk Cowham (nyk@cowham.net)
+ * @brief A library to implement a simple generic stack container
+ * @version 0.1
+ * @date 2021-04-04
+ * 
+ * @copyright Copyright (c) 2021 Nicholas Cowham
+ * 
+ */
+
+#include <stdbool.h>
+#include <stdlib.h>
+
+/**
+ * @brief value indicating an empty stack
+ * 
+ */
+#define NYK_STACK_EMPTY (-1)
+
+/**
+ * @brief 
+ * 
+ */
+typedef struct {
+    const size_t length;
+    size_t top;
+    void** items;
+} nyk_stack;
+
+/**
+ * @brief constructor to make a new stack object
+ * 
+ * @param item_size the size of each item
+ * @param stack_size the size or capacity of the stack
+ * @return nyk_stack a new stack object
+ */
+nyk_stack nyk_stack_make(size_t item_size, size_t stack_size);
+
+/**
+ * @brief add a new item to the top of the stack
+ * 
+ * @param stack a pointer to a stack object
+ * @param item a pointer to a new item to add to the stack
+ * @return true if the item was added successfully
+ * @return false if the item was not added
+ */
+bool nyk_stack_push(nyk_stack* stack, void* item);
+
+/**
+ * @brief take an item off from the top of the stack
+ * 
+ * @param stack a pointer to the stack object
+ * @return void* the next item from the top of the stack
+ */
+void* nyk_stack_pop(nyk_stack* stack);
+
+/**
+ * @brief free pointers stored on the stack before destroying it
+ * 
+ * @param stack 
+ * @return true if the stack was successfully destroyed
+ * @return false if the stack was not fully destroyed
+ */
+bool nyk_stack_destroy(nyk_stack* stack);
---
+++ datastructures/stack/src/stack.c
@@ -0,0 +1,38 @@
+#include "stack.h"
+
+nyk_stack nyk_stack_make(size_t item_size, size_t stack_length) {
+    void* stack_array = malloc(item_size * stack_length);
+    nyk_stack stack = {
+        .items = &stack_array,
+        .length = stack_length,
+        .top = NYK_STACK_EMPTY,
+    };
+
+    return stack;
+}
+
+bool nyk_stack_push(nyk_stack* stack, void* item) {
+    stack->top++;
+
+    if (stack->top < stack->length) {
+        stack->items[stack->top] = item;
+        return true;
+    }
+
+    return false;
+}
+
+void* nyk_stack_pop(nyk_stack* stack, void* item) {
+    if (stack->top == NYK_STACK_EMPTY) {
+        return NULL;
+    }
+
+    void* item = stack->items[stack->top];
+    stack->top--;
+    return item;
+}
+
+bool nyk_stack_destroy(nyk_stack* stack) {
+    stack->items = NULL;
+    free(stack->items);
+}
